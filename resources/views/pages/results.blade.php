908555bb2a8cd5d526af6d1bb50669ec4c58b41b
vps account user <devvzt@titan30.mirohost.net>
2015-12-23 17:55:38 +0200

    Html intergation serach form initial commit

--- resources/views/pages/results.blade.php
+++ resources/views/pages/results.blade.php
@@ -1,2 +1,33 @@
-<b>Всього: {{$total}}</b>
+<a class="search-form--save" href=""><i class="sprite-arrow-right"></i> Зберегти пошуковий запит</a>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-8">
+			<div class="search-form--filter">
+				<div class="result-all">{{number_format($total, 0, '', ' ')}} підходящі закупівлі</div>
+				<ul class="nav navbar-nav inline-navbar search-form--filter--filter-records">
+					<li><a href="#">Додані сьогодні</a> (?)</li>
+					<li><a href="#">Уточнення</a> (?)</li>
+					<li><a href="#">Подання пропозицій</a> (?)</li>
+					<li><a href="#">Аукціон</a> (?)</li>
+					<li><a href="#">Кваліфікація</a> (?)</li>
+					<li><a href="#">Завершені</a> (?)</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<ul class="nav navbar-nav inline-navbar search-form--filter--show-type">
+				<li>Показати:</li>
+				<li><a href="" class="active">Детально</a></li>
+				<li><a href="">Списком</a></li>
+			</ul>
+			<div class="clearfix"></div>
+			<select>
+				<option>Спочатку новіші</option>
+				<option>Спочатку новіші</option>
+			</select>
+		</div>
+	</div>
+</div>
+
 @include('partials.result')
--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,22 +1,73 @@
-<div class="wrapper">
+<nav class="navbar navbar-default top-menu">
+    <div class="container">
+        <ul class="nav navbar-nav pull-right inline-navbar">
+            <li><a href=""><i class="sprite-social-fb"></i></a></li>
+            <li><a href=""><i class="sprite-social-tw"></i></a></li>
+            <li><a href=""><i class="sprite-social-g"></i></a></li>
+            <li><a href=""><i class="sprite-social-in"></i></a></li>
+            <li><a href="">Почати роботу</a></li>
+        </ul>
+    </div>
+</nav>
+<nav class="navbar navbar-default main-menu">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="assets/images/logo.jpg" width="280" height="88" alt="Logo"></a>
+        </div>
 
-	<div class="blocks-wr">
-		<div id="blocks" class="blocks clearfix">
-			<input id="query" class="query_input no_blocks" type="text" autocomplete="off" data-js="form" data-placeholder="Назва товару, код товару, назва або ЄДРПОУ компанії, дата або що завгодно ще..."@if (!empty($preselected_values)) data-preselected='{{$preselected_values}}'@endif>
-			<button id="search_button" class="more" disabled>Шукати</button>
+		<div class="clearfix visible-sm"></div>
+		<div class="main-menu--list">
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav nav-justified">
+					<li><a href="#">ПОСТАЧАЛЬНИКУ</a></li>
+					<li><a href="#">ЗАМОВНИКУ</a></li>
+					<li><a href="#">ПРО РЕФОРМУ</a></li>
+					<li><a href="#">МОНІТОРИНГ</a></li>
+					<li><a href="#">ПІДТРИМКА</a></li>
+					<li><a href="#">НОВИНИ</a></li>
+					<li><a href="#">КОНТАКТИ</a></li>
+				</ul>
+			</div>
 		</div>
-		<div id="suggest" class="suggest"></div>
+    </div>
+</nav>
+<div class="search-form">	
+	<div class="main-search">
+		<div class="container">		
+			<h1>Пошук тендера</h1>
+			<div class="search-form--category">
+				<ul class="nav navbar-nav inline-navbar">
+					<li><a class="active" href="">Тендери</a></li>
+					<li><a href="">Планові закупівлі</a></li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+			
+			<div class="blocks-wr">
+				<div id="blocks" class="blocks clearfix">
+					<input id="query" class="query_input no_blocks" type="text" autocomplete="off" data-js="form" data-placeholder="Назва товару, код товару, назва або ЄДРПОУ компанії, дата або що завгодно ще..."@if (!empty($preselected_values)) data-preselected='{{$preselected_values}}'@endif>
+					<button id="search_button" class="more" disabled></button>
+				</div>
+				<div id="suggest" class="suggest"></div>
+			</div>
+			<div class="search-form--add-cryteria">
+				<div class="nav navbar-nav inline-navbar">
+					<div id="buttons" class="buttons"></div>
+				</div>
+				<a href="" class="pull-right">Як прискорити роботу?</a>
+			</div>
+		</div>
 	</div>
-	
+	<div class="main-result">	
+		<div id="result" class="result"></div>
+	</div>
 </div>
-<div id="buttons" class="buttons"></div>
-
-<input type="text" id="server_query" style="position:fixed;left:0px;bottom:0px;width:100%">
-
-<div id="result" style="padding:20px 20px 40px 10px;">
-</div>
-
-
 <script id="helper-suggest" type="text/x-jquery-tmpl">
 	<div class="none"><a href>{name}: <span class="highlight">{value}</span></a></div>
 </script>
