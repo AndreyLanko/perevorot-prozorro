684dd8f82f071cbd19b83258d81fa0d9eadbc275
LANKO <lanko@perevorot.com>
2016-02-12 18:33:23 +0200

    Подключение рабочего API

--- config/api.php
+++ config/api.php
@@ -3,5 +3,6 @@
 return env('APP_ENV')=='local'?[
 	'prozorro'=>env('PROZORRO_API'),
 	'ocds'=>'http://ocds-test.aws3.tk/search',
-	'sandbox'=>'http://sandbox.aws3.tk/search'
+	'sandbox'=>'http://sandbox.aws3.tk/search',
+	'search-api'=>'http://search-api.prozorro.org/search'
 ]:['prozorro'=>env('PROZORRO_API')];
