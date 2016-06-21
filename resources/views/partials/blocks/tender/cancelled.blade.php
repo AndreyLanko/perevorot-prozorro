e05f15dc36c7b3b4d4c98091cb9dc20322547601
LANKO <lanko@perevorot.com>
2016-06-21 17:36:44 +0300

    Тендер: виправлення блока про відміну

--- resources/views/partials/blocks/tender/cancelled.blade.php
+++ resources/views/partials/blocks/tender/cancelled.blade.php
@@ -4,7 +4,39 @@
             <div class="col-sm-9">
                 <h3>Інформація про відміну</h3>
 
-                @if(in_array($item->procurementMethodType, ['aboveThresholdEU', 'aboveThresholdUA', 'aboveThresholdUA.defense']) && !empty($item->__unsuccessful_awards))
+                @if($item->status=='cancelled' && !empty($item->__cancellations))
+                    @foreach($item->__cancellations as $cancellation)
+                        <div class="row">
+                            <div class="col-md-12 margin-bottom">
+                                <strong>Дата скасування</strong>
+                                <div>{{date('d.m.Y H:i', strtotime($cancellation->date))}}</div>
+                            </div>
+                            @if(!empty($cancellation->reason))
+                                <div class="col-md-12 margin-bottom">
+                                    <strong>Причина скасування</strong>
+                                    <div>{{$cancellation->reason}}</div>
+                                </div>
+                            @endif
+                            <div class="col-md-12 margin-bottom">
+                                <strong>Документи</strong>
+                                @if (!empty($cancellation->documents))
+                                    <table class="tender--customer">
+                                        <tbody>
+                                            @foreach($cancellation->documents as $k=>$document)
+                                                <tr>
+                                                    <td class="col-sm-2" style="padding-left:0px;">{{!empty($document->dateModified) ? date('d.m.Y H:i', strtotime($document->dateModified)) : trans('tender.no_date')}}</td>
+                                                    <td class="col-sm-6"><a href="{{$document->url}}" target="_blank" class="word-break{{!empty($document->stroked) ? ' stroked': ''}}">{{$document->title}}</a></td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                @else
+                                    <div class="tender--customer padding-td">{{trans('tender.no_documents')}}</div>
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
+                @elseif(in_array($item->procurementMethodType, ['aboveThresholdEU', 'aboveThresholdUA', 'aboveThresholdUA.defense']) && !empty($item->__unsuccessful_awards))
                     <div class="row">
                         <div class="col-md-12 margin-bottom">
                             <strong>Дата відміни</strong>
@@ -16,44 +48,31 @@
                         </div>
                     </div>
                 @else
-                    @if($item->status=='cancelled')
-                        @if(!empty($item->__cancellations))
-                            @foreach($item->__cancellations as $cancellation)
-                                <div class="row">
-                                    <div class="col-md-12 margin-bottom">
-                                        <strong>Дата скасування</strong>
-                                        <div>{{date('d.m.Y H:i', strtotime($cancellation->date))}}</div>
-                                    </div>
-                                    @if(!empty($cancellation->reason))
-                                        <div class="col-md-12 margin-bottom">
-                                            <strong>Причина скасування</strong>
-                                            <div>{{$cancellation->reason}}</div>
-                                        </div>
-                                    @endif
-                                    <div class="col-md-12 margin-bottom">
-                                        <strong>Документи</strong>
-                                        @if (!empty($cancellation->documents))
-                                            <table class="tender--customer">
-                                                <tbody>
-                                                    @foreach($cancellation->documents as $k=>$document)
-                                                        <tr>
-                                                            <td class="col-sm-2" style="padding-left:0px;">{{!empty($document->dateModified) ? date('d.m.Y H:i', strtotime($document->dateModified)) : trans('tender.no_date')}}</td>
-                                                            <td class="col-sm-6"><a href="{{$document->url}}" target="_blank" class="word-break{{!empty($document->stroked) ? ' stroked': ''}}">{{$document->title}}</a></td>
-                                                        </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                        @else
-                                            <div class="tender--customer padding-td">{{trans('tender.no_documents')}}</div>
-                                        @endif
-                                    </div>
-                                </div>
-                            @endforeach
-                        @endif
-                    @endif
-                    
+                    <?php
+                        $numberOfBids=0;
+
+                        if(!empty($item->__bids))
+                        {
+                            $numberOfBids=array_where($item->__bids, function($key, $bid){
+                                return !empty($bid->status) && $bid->status=='active';
+                            });
+                        
+                            $numberOfBids=$numberOfBids ? sizeof($numberOfBids) : 0;
+                        }
+
+                        $numberOfQualifications=0;
+
+                        if(!empty($item->__qualifications))
+                        {
+                            $numberOfQualifications=array_where($item->__qualifications, function($key, $qualification){
+                                return !empty($qualification->status) && $qualification->status=='active';
+                            });
+                            
+                            $numberOfQualifications=$numberOfQualifications ? sizeof($numberOfQualifications) : 0;
+                        }
+                    ?>
                     @if($item->status=='unsuccessful')
-                        @if($item->procurementMethodType=='aboveThresholdUA')
+                        @if(in_array($item->procurementMethodType, ['aboveThresholdUA', 'aboveThresholdUA.defense']))
                             <div class="row">
                                 <div class="col-md-12 margin-bottom">
                                     <strong>Дата відміни</strong>
@@ -86,10 +105,10 @@
                                     <div>подання для участі в торгах менше двох тендерних пропозицій</div>
                                 </div>
                             </div>                        
-                        @elseif($item->procurementMethodType=='aboveThresholdEU' && $numberOfBids >= 2)
+                        @elseif($item->procurementMethodType=='aboveThresholdEU' && $numberOfQualifications < 2)
                             <div class="row">
                                 <div class="col-md-12 margin-bottom">
-                                    <strong>Дата відміни, {{$numberOfBids}}</strong>
+                                    <strong>Дата відміни</strong>
                                     <div>
                                         @if(!empty($qualificationPeriod->endDate))
                                             {{date('d.m.Y H:i', strtotime($qualificationPeriod->endDate))}}
@@ -103,7 +122,7 @@
                                     <div>допущення до оцінки менше двох тендерних пропозицій</div>
                                 </div>
                             </div>
-                        @else                        
+                        @else
                             <div class="row">
                                 <div class="col-md-12 margin-bottom">
                                     <strong>Торги відмінено</strong>
