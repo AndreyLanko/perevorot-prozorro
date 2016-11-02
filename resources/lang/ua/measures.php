da14458813b7f80c02ffe3e01b02d71fe7d23fad
LANKO <lanko@perevorot.com>
2016-11-02 20:16:10 +0200

    Синхронізування одиниць виміру за http://standards.openprocurement.org/unit_codes/recommended/uk.json

--- resources/lang/ua/measures.php
+++ resources/lang/ua/measures.php
@@ -1,115 +1,21 @@
 <?php
+    $unit_codes=\Cache::remember('unit_codes', 60*24, function(){
+        $u=file_get_contents('http://standards.openprocurement.org/unit_codes/recommended/uk.json');
+        $out=[];
 
-return [
-    
-    "PR"=> [
-        "symbol"=>"пар.",
-        "name"=>"пара"
-    ],
-    "LTR"=> [
-        "symbol"=>"л.",
-        "name"=>"літр"
-    ],
-    "SET"=> [
-        "symbol"=>"наб.",
-        "name"=>"набір"
-    ],
-    "RM"=> [
-        "symbol"=>"пач.",
-        "name"=>"пачка"
-    ],
-    "PK"=> [
-        "symbol"=>"упак.",
-        "name"=>"упаковка"
-    ],
-    "NMP"=> [
-        "symbol"=>"пач.",
-        "name"=>"пачок"
-    ],
-    "MTR"=> [
-        "symbol"=>"м",
-        "name"=>"метри"
-    ],
-    "BX"=> [
-        "symbol"=>"ящ.",
-        "name"=>"ящик"
-    ],
-    "E48"=> [
-        "symbol"=>"посл.",
-        "name"=>"послуга"
-    ],
-    "MTQ"=> [
-        "symbol"=>"м.куб.",
-        "name"=>"метри кубічні"
-    ],
-    "E54"=> [
-        "symbol"=>"рейс",
-        "name"=>"рейс"
-    ],
-    "TNE"=> [
-        "symbol"=>"т.",
-        "name"=>"тони"
-    ],
-    "MTK"=> [
-        "symbol"=>"м.кв.",
-        "name"=>"метри квадратні"
-    ],
-    "KMT"=> [
-        "symbol"=>"км",
-        "name"=>"кілометри"
-    ],
-    "H87"=> [
-        "symbol"=>"шт.",
-        "name"=>"штуки"
-    ],
-    "MON"=> [
-        "symbol"=>"місяць",
-        "name"=>"місяць"
-    ],
-    "LO"=> [
-        "symbol"=>"лот",
-        "name"=>"лот"
-    ],
-    "D64"=> [
-        "symbol"=>"блок",
-        "name"=>"блок"
-    ],
-    "HAR"=> [
-        "symbol"=>"га.",
-        "name"=>"гектар"
-    ],
-    "KGM"=> [
-        "symbol"=>"кг.",
-        "name"=>"кілограми"
-    ],
-    'R9'=>[
-        'symbol'=>'т.м.куб',
-        'name'=>'тисяча кубічних метрів'
-    ],
-    'KT'=>[
-        'symbol'=>'комп.',
-        'name'=>'комплект'
-    ],
-    'E51'=>[
-        'symbol'=>'роб.',
-        'name'=>'роботи'
-    ],
-    'kW'=>[
-        'h'=>[
-            'symbol'=>'кВт⋅год',
-            'name'=>'кіловат-година'
-        ]
-    ],
-    'KWH'=>[
-        'symbol'=>'кВт⋅год',
-        'name'=>'кіловат-година'        
-    ],
-    'E11'=>[
-        'symbol'=>'Гкал',
-        'name'=>'гігакалорія'        
-    ],
-    'LM'=>[
-        'symbol'=>'пог.м.',
-        'name'=>'Погонний метр'        
-    ],
-];
+        if($u){
+            $array=json_decode($u, true);
+            
+            foreach($array as $key=>$one)
+            {
+                $out[$key]=[
+                    'symbol' => $one['symbol_uk'],
+                    'name' => $one['name_uk']
+                ];
+            }
+        }
+
+        return $out;
+    });
+
+    return $unit_codes;
